6c0f497790cbbabeab4abb84995f4f9a00d7e421
Renanjuniior6 <renanjrr679@gmail.com>
2024-12-17 16:29:36 -0300

    first commit

---
+++ src/app/driver/MapDriver.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { useRef } from "react";
+import { useMap } from "@/hooks/useMap";
+
+
+export function MapDriver() {
+
+  const mapContainerRef = useRef<HTMLDivElement>(null);
+  useMap(mapContainerRef);
+
+
+  return <div className="w-2/3 h-full" ref={mapContainerRef} />;
+}
